b0adb3b1b5ba4f4be9c6b0b0ce83374692c85fb1
denxorz <denxorz@gmail.com>
2015-06-28 17:04:31 +0200

    Rename files

--- ChocolateyMilk/Properties/AssemblyInfo.cs
+++ ChocolateyMilk/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApplication1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ChocolateyMilk")]
+[assembly: AssemblyDescription("Gui for Chocolatey")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApplication1")]
+[assembly: AssemblyCompany("Denxorz")]
+[assembly: AssemblyProduct("ChocolateyMilk")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
