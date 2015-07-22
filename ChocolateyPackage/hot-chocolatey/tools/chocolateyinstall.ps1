034a53c803156068b232ae55bae6c501198a2015
denxorz <denxorz@gmail.com>
2015-07-22 20:43:59 +0200

    Add Chocolatey package info

---
+++ ChocolateyPackage/hot-chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'hot-chocolatey'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://hotchocolatey.jjb3.nl/1.0.0.0/Setup%20Hot%20Chocolatey.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  silentArgs    = "/qn /norestart /l*v '$env:TEMP\chocolatey\$packageName\install.log'"
+  validExitCodes= @(0, 3010, 1641)
+  registryUninstallerKey = 'hot-chocolatey'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ ChocolateyPackage/hot-chocolatey/tools/chocolateyuninstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'hot-chocolatey'
+$registryUninstallerKeyName = 'hot-chocolatey'
+$msiProductCodeGuid = 'http://hotchocolatey.jjb3.nl/1.0.0.0/Setup%20Hot%20Chocolatey.msi'
+$shouldUninstall = $true
+
+$local_key     = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName" 
+$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+$file = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty UninstallString
+
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'MSI' 
+$silentArgs = "$msiProductCodeGuid /qn /norestart"
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+$file = ''
+
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+}
