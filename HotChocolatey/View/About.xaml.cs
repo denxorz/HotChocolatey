9c717a8a6285b2e949daefc71d4aefb4d804f953
denxorz <denxorz@gmail.com>
2015-07-28 20:26:37 +0200

    Add missing file

---
+++ HotChocolatey/View/About.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace HotChocolatey.View
+{
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+    }
+}
