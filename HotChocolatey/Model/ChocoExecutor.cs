5ba545da788659af22775fc32f5cad7e5fa0fffc
denxorz <denxorz@gmail.com>
2016-06-04 17:33:22 +0200

    Moved Execute method

--- HotChocolatey/Model/ChocoExecutor.cs
+++ HotChocolatey/Model/ChocoExecutor.cs
@@ -1,8 +1,6 @@
-﻿using HotChocolatey.Utility;
-using NuGet;
+﻿using NuGet;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using HotChocolatey.Model.ChocoTask;
@@ -18,60 +16,29 @@ namespace HotChocolatey.Model
         {
             LocalPackages.Clear();
             repo.ClearLocalVersions();
-            await new UpdateLocalChocoTask(IncludePreReleases, repo, LocalPackages.Add).Execute(this);
-            await new UpdateOutdatedFlagsChocoTask(repo).Execute(this);
+            await new UpdateLocalChocoTask(IncludePreReleases, repo, LocalPackages.Add).Execute();
+            await new UpdateOutdatedFlagsChocoTask(repo).Execute();
             await UpdateNuGetInfo(nuGetExecutor);
         }
 
         public async Task Install(Package package, SemanticVersion specificVersion, Action<string> outputLineCallback)
         {
-            await new InstallChocoTask(outputLineCallback, IncludePreReleases, package, specificVersion).Execute(this);
+            await new InstallChocoTask(outputLineCallback, IncludePreReleases, package, specificVersion).Execute();
         }
 
         public async Task Uninstall(Package package, Action<string> outputLineCallback)
         {
-            await new UninstallChocoTask(outputLineCallback, package).Execute(this);
+            await new UninstallChocoTask(outputLineCallback, package).Execute();
         }
 
         public async Task Upgrade(Package package, SemanticVersion specificVersion, Action<string> outputLineCallback)
         {
-            await new UpgradeChocoTask(outputLineCallback, IncludePreReleases, package, specificVersion).Execute(this);
+            await new UpgradeChocoTask(outputLineCallback, IncludePreReleases, package, specificVersion).Execute();
         }
 
         private async Task UpdateNuGetInfo(NuGetExecutor nuGetExecutor)
         {
             await Task.WhenAll(LocalPackages.Select(t => Task.Run(() => nuGetExecutor.Update(t))));
-        }
-
-        public async Task<bool> Execute(string arguments, Action<string> outputLineCallback)
-        {
-            Log.Info($">> choco {arguments}");
-
-            using (Process choco = new Process())
-            {
-                choco.StartInfo.FileName = "choco";
-                choco.StartInfo.Arguments = arguments;
-                choco.StartInfo.UseShellExecute = false;
-                choco.StartInfo.RedirectStandardOutput = true;
-                choco.StartInfo.CreateNoWindow = true;
-                choco.Start();
-
-                bool endOfStream = false;
-                while (!endOfStream)
-                {
-                    string line = await choco.StandardOutput.ReadLineAsync();
-                    Log.Info($"> {line}");
-                    outputLineCallback(line);
-
-                    await Task.Run(() => endOfStream = choco.StandardOutput.EndOfStream);
-                }
-
-                if (choco.ExitCode != 0)
-                {
-                    Log.Error($">> choco {arguments} exited with code {choco.ExitCode}");
-                }
-                return choco.ExitCode == 0;
-            }
         }
     }
 }
--- HotChocolatey/Model/ChocoTask/BaseChocoTask.cs
+++ HotChocolatey/Model/ChocoTask/BaseChocoTask.cs
@@ -1,19 +1,52 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using HotChocolatey.Utility;
 
 namespace HotChocolatey.Model.ChocoTask
 {
     internal abstract class BaseChocoTask
     {
-        public async Task Execute(ChocoExecutor executor)
+        public async Task Execute()
         {
-            bool result = await executor.Execute($"{GetCommand()} --limitoutput {GetParameters()}", GetOutputLineCallback());
+            bool result = await Execute($"{GetCommand()} --limitoutput {GetParameters()}", GetOutputLineCallback());
             AfterExecute(result);
         }
 
         protected abstract string GetCommand();
         protected abstract string GetParameters();
         protected abstract Action<string> GetOutputLineCallback();
+
+        private async Task<bool> Execute(string arguments, Action<string> outputLineCallback)
+        {
+            Log.Info($">> choco {arguments}");
+
+            using (Process choco = new Process())
+            {
+                choco.StartInfo.FileName = "choco";
+                choco.StartInfo.Arguments = arguments;
+                choco.StartInfo.UseShellExecute = false;
+                choco.StartInfo.RedirectStandardOutput = true;
+                choco.StartInfo.CreateNoWindow = true;
+                choco.Start();
+
+                bool endOfStream = false;
+                while (!endOfStream)
+                {
+                    string line = await choco.StandardOutput.ReadLineAsync();
+                    Log.Info($"> {line}");
+                    outputLineCallback(line);
+
+                    await Task.Run(() => endOfStream = choco.StandardOutput.EndOfStream);
+                }
+
+                if (choco.ExitCode != 0)
+                {
+                    Log.Error($">> choco {arguments} exited with code {choco.ExitCode}");
+                }
+                return choco.ExitCode == 0;
+            }
+        }
 
         protected virtual void AfterExecute(bool result)
         {
