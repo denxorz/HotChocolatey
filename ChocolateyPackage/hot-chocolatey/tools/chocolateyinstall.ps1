d1911652a1072931c202bf88fd5b2c22b39267c7
denxorz <denxorz@gmail.com>
2016-10-16 19:42:40 +0200

    Fix checksum

--- ChocolateyPackage/hot-chocolatey/tools/chocolateyinstall.ps1
+++ ChocolateyPackage/hot-chocolatey/tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 ﻿$name   = 'hot-chocolatey'
 $url = 'http://hotchocolatey.jjb3.nl/releases/__version__/Setup%20Hot%20Chocolatey.msi'
 $silent = '/quiet'
+$checksum = '__checksum__'
+$checksumType = 'sha512'
 
-Install-ChocolateyPackage $name 'msi' $silent $url
+Install-ChocolateyPackage $name 'msi' $silent $url -checksum $checksum -checksumType $checksumType
