c13fcb3ff7c59c6463c8b3d95620e77f3df5acbf
Denxorz <jjb3nl@gmail.com>
2017-10-07 18:58:55 +0200

    Application now keeps running in tray

--- HotChocolatey/View/Main/MainWindow.xaml.cs
+++ HotChocolatey/View/Main/MainWindow.xaml.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Drawing;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
+using ToolBar = System.Windows.Controls.ToolBar;
 
 namespace HotChocolatey.View.Main
 {
     public partial class MainWindow
     {
+        private NotifyIcon notifyIcon;
+        private bool actualClose;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,10 +38,17 @@ namespace HotChocolatey.View.Main
         {
             // TODO : Not really neat, but good enough for now
             ((ViewModel.MainWindowViewModel)DataContext).Loaded();
-            ((ViewModel.MainWindowViewModel) DataContext).RequestBringToFront += RequestBringToFront;
+            ((ViewModel.MainWindowViewModel)DataContext).RequestBringToFront += RequestBringToFront;
+
+            StartTrayTool();
         }
 
         private void RequestBringToFront(object sender, EventArgs e)
+        {
+            BringToFront();
+        }
+
+        private void BringToFront()
         {
             if (!IsVisible)
             {
@@ -47,8 +61,60 @@ namespace HotChocolatey.View.Main
             }
 
             Activate();
-            Topmost = true;  // important
+            Topmost = true; // important
             Topmost = false; // important
+        }
+
+        private void StartTrayTool()
+        {
+            var applicationName = Application.Current.MainWindow.Title;
+
+            var contextMenuStrip = new ContextMenuStrip();
+            contextMenuStrip.SuspendLayout();
+
+            var exitToolStripMenuItem = new ToolStripMenuItem { Text = "Exit" };
+            exitToolStripMenuItem.Click += (s, e) =>
+            {
+                actualClose = true;
+                Close();
+            };
+
+            var openToolStripMenuItem = new ToolStripMenuItem { Text = $"Show {applicationName}..." };
+            openToolStripMenuItem.Font = new Font(openToolStripMenuItem.Font, openToolStripMenuItem.Font.Style | System.Drawing.FontStyle.Bold);
+            openToolStripMenuItem.Click += RequestBringToFront;
+
+            notifyIcon = new NotifyIcon
+            {
+                ContextMenuStrip = contextMenuStrip,
+                Icon = new Icon(Application.GetResourceStream(new Uri("pack://application:,,,/Hot Chocolate-96.ico")).Stream),
+                Text = applicationName,
+                Visible = true
+            };
+            notifyIcon.MouseClick += (s, e) =>
+            {
+                if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+                {
+                    BringToFront();
+                }
+            };
+
+            contextMenuStrip.Items.AddRange(new ToolStripItem[]
+            {
+                openToolStripMenuItem,
+                new ToolStripSeparator(),
+                exitToolStripMenuItem
+            });
+            contextMenuStrip.Size = new System.Drawing.Size(179, 70);
+            contextMenuStrip.ResumeLayout(false);
+        }
+
+        private void OnClosing(object sender, CancelEventArgs e)
+        {
+            if (!actualClose)
+            {
+                Hide();
+                e.Cancel = true;
+            }
         }
     }
 }
